8b05c19afa4bda1dd18064cb17fea1866b54ed7b
shanghanrun <idim7@naver.com>
2023-07-11 18:22:28 +0900

    first commit

---
+++ android/app/src/main/kotlin/com/choco/video_call/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.choco.video_call
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:video_call/screen/home_screen.dart';
+
+void main() {
+  runApp(const MaterialApp(home: HomeScreen()));
+}
